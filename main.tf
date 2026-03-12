3517b4e1a326fed2cfad6bc51c63dfc20a2a7a87
PartySlayer <squadrittichristian@gmail.com>
2026-03-12 11:32:00 +0100

    ++ root adeguata per testare la rete centralizzata

--- main.tf
+++ main.tf
@@ -26,3 +26,30 @@ module "inspection_vpc" {
   az_2   = var.az_2
   tgw_id = aws_ec2_transit_gateway.this.id
 }
+
+# Adesso che abbiamo l'hub centrale e la vpc di ispezione nel mezzo,
+# dobbiamo collegare gli "spokes", le VPC dedicate ai workload
+
+# Istanziamo la Workload VPC 1
+module "workload_vpc_1" {
+  source = "./modules/workload_vpc"
+
+  vpc_name        = "workload-1"
+  vpc_cidr        = "10.1.0.0/16"
+  app_subnet_cidr = "10.1.1.0/24"
+  tgw_subnet_cidr = "10.1.2.0/28" # Basta una /28 per il TGW, anzi meglio per non sprecare IP utili
+  az              = var.az
+  tgw_id          = aws_ec2_transit_gateway.this.id
+}
+
+# Istanziamo la Workload VPC 2
+module "workload_vpc_2" {
+  source = "./modules/workload_vpc"
+
+  vpc_name        = "workload-2"
+  vpc_cidr        = "10.2.0.0/16"
+  app_subnet_cidr = "10.2.1.0/24"
+  tgw_subnet_cidr = "10.2.2.0/28" # Stessa cosa qui
+  az              = var.az
+  tgw_id          = aws_ec2_transit_gateway.this.id
+}
